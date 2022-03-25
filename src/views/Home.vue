a74d68f18b3a484dc792730d1d8b40cad6a43eda
WilliamCauich10 <william.cauich10@gmail.com>
2022-03-25 09:41:13 -0500

    Modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,27 @@
     <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
     <button @click="goToDaybook" class="btn btn-success">success</button>
   </div>
+
+  <br>
+
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+  </button>
+
+  <!-- Modal -->
+  <Modal />
+
+
 </template>
 
 
 <script>
+import Modal from "./Modal.vue";
 export default {
+  components: {
+    Modal
+  },
   methods: {
     goToDaybook() {
       this.$router.push({ name: 'no-entry' })
